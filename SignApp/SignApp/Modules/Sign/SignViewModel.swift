c0a8649681422e84f2153bed4a7d59ecfca4f60e
Artem <artem@wildapricot.com>
2021-05-01 15:24:45 +0300

    cobmine password and confirm password to avoid bug

--- SignApp/SignApp/Modules/Sign/SignViewModel.swift
+++ SignApp/SignApp/Modules/Sign/SignViewModel.swift
@@ -28,6 +28,7 @@ class SignViewModel: ObservableObject {
         
         $password
             .debounce(for: 1, scheduler: DispatchQueue.main)
+            .removeDuplicates()
             .map({ [unowned self] in
                 // print("<<<DEV>>> isPasswordValid \(isPasswordValid($0))")
                 return self.isPasswordValid($0)
@@ -35,25 +36,24 @@ class SignViewModel: ObservableObject {
             .assign(to: \.isPasswordValid, on: self)
             .store(in: &subscriptions)
         
-        $confirmPassword
+        $confirmPassword.combineLatest($password)
             .debounce(for: 1, scheduler: DispatchQueue.main)
-            .map({ [unowned self] in
-                // print("<<<DEV>>> arePasswordsEqual \(arePasswordsEqual($0))")
-                
-                return self.arePasswordsEqual($0)
+            .map({ combinedValue in
+                return combinedValue.0 == combinedValue.1
             })
+            .removeDuplicates()
             .assign(to: \.arePasswordsEqual, on: self)
             .store(in: &subscriptions)
         
         $isUserNameValid.combineLatest($isPasswordValid, $arePasswordsEqual)
             .map({ combinedValue in
-                let result = combinedValue.0 && combinedValue.1 && combinedValue.2
-                print("<<<DEV>>> isUserNameValid = \(combinedValue.0)")
-                print("<<<DEV>>> isPasswordValid = \(combinedValue.1)")
-                print("<<<DEV>>> arePasswordsEqual = \(combinedValue.2)")
-                return result
+                // print("<<<DEV>>> isUserNameValid = \(combinedValue.0)")
+                // print("<<<DEV>>> isPasswordValid = \(combinedValue.1)")
+                // print("<<<DEV>>> arePasswordsEqual = \(combinedValue.2)")
+                return combinedValue.0 && combinedValue.1 && combinedValue.2
                 
             })
+            .removeDuplicates()
             .assign(to: \.areUserCredentialsValid, on: self)
             .store(in: &subscriptions)
     }
@@ -69,7 +69,7 @@ class SignViewModel: ObservableObject {
             .subscribe(on: DispatchQueue.global())
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: { [unowned self] value in
-                print("<<<DEV>>> Receive validation result in thread \(Thread.current) \(value)")
+                // print("<<<DEV>>> Receive validation result in thread \(Thread.current) \(value)")
                 
                 self.isUserNameValid = value
             })
@@ -84,13 +84,5 @@ class SignViewModel: ObservableObject {
         return password.count > 8
             // && password != "admin" TODO[ARTEM]: Remove unnecessary checks
             // && password != "password"
-    }
-    
-    private func arePasswordsEqual(_ password: String) -> Bool {
-        guard !password.isEmpty else {
-            return false
-        }
-        
-        return password == self.password
     }
 }
