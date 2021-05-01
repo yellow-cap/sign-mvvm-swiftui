36023c43d68b7eaa06b1105ab597ce430fb1d4ed
Artem <artem@wildapricot.com>
2021-05-01 10:15:29 +0300

    add fetcher mock

--- SignApp/SignApp/Api/AccountFetcherMock.swift
+++ SignApp/SignApp/Api/AccountFetcherMock.swift
@@ -6,9 +6,13 @@ protocol IAccountFetcher {
 }
 
 class AccountFetcherMock: IAccountFetcher {
+    let networkDelayInSeconds = 1
+    
     func validateUserName(userName: String) -> Future<Bool, Never> {
-        return Future { promise in
-            DispatchQueue.global().asyncAfter(deadline: .now() + 5) {
+        return Future { [weak self] promise in
+            guard let self = self else { return }
+            
+            DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(self.networkDelayInSeconds)) {
                 print("<<<DEV>>> Run validation in thread \(Thread.current)")
                 promise(.success(ServerMock.validateUserName(userName)))
             }
